f686399dadd69bdd0cd2f5356584e4496c7418d1
Quan Duong <ruathudo@gmail.com>
2018-07-18 22:03:56 +0300

    prepare remove flask resplus, use flask apispec instead

--- app/apis/auth.py
+++ app/apis/auth.py
@@ -4,9 +4,25 @@ from app.models import User
 api = Namespace('auth')
 
 
-@api.route('/login')
-class Login(Resource):
+@api.route('/')
+class Auth(Resource):
 
     def post(self):
-        """User authentication"""
+        """User Login"""
+        return {"success": True}
+
+    def delete(self):
+        """User logout"""
+        return {"success": True}
+
+
+@api.route('/reset')
+class ResetPassword(Resource):
+
+    def post(self):
+        """Send forget password email"""
+        return {"success": True}
+
+    def get(self):
+        """Reset password"""
         return {"success": True}
--- app/apis/user.py
+++ app/apis/user.py
@@ -1,13 +1,33 @@
-from flask_restplus import Namespace, Resource, fields
+from flask_restplus import Namespace, Resource
+from webargs import fields
+from webargs.flaskparser import use_args
 from app.models import User
 
 api = Namespace('users')
 
+user_args = {
+    'name': fields.Str(required=True)
+}
 
-@api.route('/<id>')
-class UserAPI(Resource):
 
-    def get(self, uid):
+@api.route('/<int:id>')
+class UserSingle(Resource):
+
+    def get(self, id):
         """Get user info"""
-        return User.query.filter_by(id=uid).all()
+        return User.query.filter_by(id=id).all()
 
+    def put(self):
+        """Update user info"""
+        return 200
+
+
+@api.route('/')
+class UserList(Resource):
+
+    @api.doc(params={'name': 'fullname'})
+    @use_args(user_args)
+    def post(self, args):
+        """Create new user"""
+        print(args)
+        return 201
